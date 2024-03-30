3d4c0e604b69a78d27adb18e760ef5e5c870858a
suchitathorat <suchitathorat73@gmail.com>
2024-03-30 11:55:17 +0530

    errors are solved

--- client/src/views/Home/Home.js
+++ client/src/views/Home/Home.js
@@ -1,23 +1,21 @@
-import React, { useEffect, useState } from 'react'
+import React, {useEffect, useState} from 'react'
 import './Home.css'
 import axios from 'axios'
 
-
 function Home() {
-    const[notes ,setNotes]=useState([]);
+    const [notes ,setNotes]=useState([])
 
     const loadNotes=async()=>{
-        const response =await axios.get(`${process.env.REACT_APP_API_URL}notes`);
-    
+        const response=await axios.get('http://localhost:5000/notes');
 
         console.log(response.data.data);
-
         setNotes(response.data.data);
-    }
+    };
 
     useEffect(()=>{
-        loadNotes()
-    },[])
+        loadNotes();
+    },[]);
+
 
 
 
@@ -27,15 +25,16 @@ function Home() {
         <h1>Home</h1>
 
         {
-            notes.map((note,index)=>{
-                return(
-                    <div>
-                        <h3>{note.title}</h3>
-                        <p>{note.content}</p>
-                        <span>{note.category}</span>
-                    </div>
-                )
-            })
+          notes.map((note,index)=>{
+            return(
+              <div>
+                <h3>{note.title}</h3>
+                <p>{note.content}</p>
+                <span>{note.category}</span>
+              </div>
+            )
+          })
+
         }
     </div>
   )
